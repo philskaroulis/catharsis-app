bbf3520e264379b402bf9b4fda6fcbd4dc7d6a49
Phil Skaroulis <phil@skaroulis.com>
2013-04-17 00:18:36 -0400

    added more mock data. cleaned up the index. added playlist link and section.

--- app/js/app.js
+++ app/js/app.js
@@ -6,6 +6,40 @@ angular.module('catharsisApp', ['catharsisApp.filters', 'catharsisApp.services',
 config(['$routeProvider', function($routeProvider) {
 
     $routeProvider.when('/movies/guys/fights', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/guys/carchases', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/guys/robberies', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/guys/war', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/guys/sports', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/guys/westerns', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+
+    $routeProvider.when('/movies/ladies/musicals', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/ladies/comedy', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/ladies/romance', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/ladies/adventure', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/ladies/drama', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/ladies/spituality', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+
+    $routeProvider.when('/movies/kids/animation', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/kids/adventure', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/kids/fantasy', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/kids/scifi', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/kids/mystery', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/kids/comedy', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+
+    $routeProvider.when('/movies/adults/drama', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/adults/adventure', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/adults/fantasy', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/adults/scifi', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/adults/westerns', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/adults/comedy', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+
+    $routeProvider.when('/movies/family/family', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/family/adventure', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/family/fantasy', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/family/scifi', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/family/mystery', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+    $routeProvider.when('/movies/family/comedy', {templateUrl: 'partials/movie-list.html', controller: 'MovieListController'});
+
     $routeProvider.otherwise({redirectTo: '/'});
 
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,12 +6,23 @@ angular.module('catharsisApp.controllers', []).
     controller('MovieListController', ['$scope',function($scope) {
 
         $scope.movies = [
+
             {title:'Little Big Man', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/tWP2LoaMRjX3Y4dDOSwE2prN423.jpg',date:'1970-12-14',added:false},
             {title:'On the Waterfront', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/zXp2ydvhO9qGzpIsb1CWeKnn5yg.jpg',date:'1954-07-28',added:false},
             {title:'Princess Mononoke', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/dB2rATwfCbsPGfRLIoluBnKdVHb.jpg',date:'1997-07-12',added:false},
-            {title:'Crying Freeman', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/9a4il5LEio4cEgbAgJe8fkJfBYB.jpg',date:'1995-09-13',added:false},
+
+            {title:'Death Wish', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/motRXAGirCsQArcQICxoI8pZ5pQ.jpg',date:'1974-07-24',added:false},
             {title:'The Notebook', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/qsyf0GobJbE4k5ljOPTQWT8fmAx.jpg',date:'2004-06-25',added:false},
-            {title:'Redline', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/kFh9LOeCEiPeSWJToPwoVvPApFc.jpg',date:'2009-08-14',added:false}
+            {title:'Redline', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/kFh9LOeCEiPeSWJToPwoVvPApFc.jpg',date:'2009-08-14',added:false},
+
+            {title:'Days of Being Wild', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/8SSIVqKnjv7UGITEemFwEQ0j9iS.jpg',date:'1990-12-15',added:false},
+            {title:'Silverado', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/v6YHlUoqZsCyFnB8utdbWyTgWlW.jpg',date:'1985-07-10',added:false},
+            {title:'Curse of the Golden Flower', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/4RUDmfAojukZ3QmKsoXnVHa6wmQ.jpg',date:'2006-12-21',added:false},
+
+            {title:'Rocky Balboa', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/1hCngs2CrxCZaPTwvxT9le7lJbO.jpg',date:'2006-12-20',added:false},
+            {title:'La Guerre Des Boutons', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/dty4DX4mrhGBs2GIyEpp6Gxk9PM.jpg',date:'1962-04-18',added:false},
+            {title:'Jennifer 8', imgUrl:'http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w500/l9O3n1KF528SqPTzGrRjzYYgAS7.jpg',date:'1992-11-06',added:false}
+
         ];
 
         $scope.toggleMembership = function(movie) {
@@ -19,14 +30,19 @@ angular.module('catharsisApp.controllers', []).
         };
 
     }]).
-    controller('BreadcrumbController', ['$scope',function($scope) {
+    controller('TopBarController', ['$scope',function($scope) {
 
-        $scope.styleBreadcrumb = 'display:none';
+        $scope.titleStyle = 'display:none';
 
         $scope.setTitle = function(genre,keyword) {
             $scope.genre = ' :: ' + genre;
             $scope.keyword =' :: ' + keyword;
-            $scope.styleBreadcrumb = 'display:inline';
+            $scope.titleStyle = 'display:inline';
         };
+
+    }]).
+    controller('PlaylistController', ['$scope',function($scope) {
+
+        $scope.playlistSize = 0;
 
     }]);
